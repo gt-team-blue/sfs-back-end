f3f1ba9c07f0d93a9d2a985c8ee942d8445cbaf5
Phillip Stephens <pstephens9275@gmail.com>
2019-09-04 14:45:47 -0400

    Updated keys.js to point to SFS_Backend in the Atlas cluster

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/test"
+    "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/SFS_Backend"
 };
 
 module.exports = {
-  mongoURI: "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/test",
+  mongoURI: "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/SFS_Backend",
   secretOrKey: "secret"
 };
