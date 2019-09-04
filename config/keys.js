d30f582697e97867eb4259d08ecc473939a7dd95
Phillip Stephens <pstephens9275@gmail.com>
2019-09-04 14:38:02 -0400

    Node.js server now responds to login and register PUT requests. Connects with database

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,8 @@ module.exports = {
   mongoURI:
     "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/test"
 };
+
+module.exports = {
+  mongoURI: "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/test",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+//Taken from: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const passport = require("passport");
+const users = require("./routes/api/users");
+
 const app = express();
 // Bodyparser middleware
 app.use(
@@ -16,5 +23,13 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
