1de42f3d09cc988b86637f7afe08de3d3f7864ad
Tim Proctor <timproctor@gatech.edu>
2019-09-16 16:09:28 -0400

    Added stories

--- routes/api/stories.js
+++ routes/api/stories.js
@@ -4,14 +4,9 @@ const router = express.Router();
 const Story = require("../../models/Story");
 
 router.get('/story', (req, res) => {
-    // res.send({type: 'GET'});
     Story.find({}).then(function(stories){
         res.send(stories);
     });
-    // Story.find((err, data) => {
-    //   if (err) return res.json({ success: false, error: err });
-    //   return res.json({ success: true, data: data });
-    // });
 });
 
 module.exports = router;
