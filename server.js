9a0961d03535d2ad44163a73c036411fe8cfe596
aidancbrady <me@aidancbrady.com>
2019-09-23 11:46:59 -0400

    GridFS back-end logic, untested

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const users = require("./routes/api/users");
 const stories = require("./routes/api/stories");
 
 const app = express();
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -21,7 +22,10 @@ const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB successfully connected"))
+  .then(() => {
+      console.log("MongoDB successfully connected")
+      initGridFS()
+  })
   .catch(err => console.log(err));
 
 // Passport middleware
@@ -33,4 +37,57 @@ app.use("/api/users", users);
 app.use("/api/stories", stories);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.log('Server up and running on port ${port}!'));
+
+function initGridFS() {
+    // PDF file management
+    let multer = require('multer')
+    let GridFsStorage = require('multer-gridfs-storage')
+    let Grid = require('gridfs-stream')
+    Grid.mongo = mongoose.mongo
+    let gfs = Grid(mongoose.connection.db)
+    console.log('Initialized GridFS successfully!')
+    let storage = GridFsStorage({
+        gfs: gfs,
+        db: mongoose.connection.db,
+        filename: (req, file, cb) => {
+            let date = Date.now()
+            cb(null, file.fieldname + '-' + date + '.')
+        },
+        metadata: function(req, file, cb) {
+            cb(null, {originalname: file.originalname})
+        },
+        root: 'ctFiles'
+    })
+    let upload = multer({
+        storage: storage
+    }).single('file')
+
+    app.post('/upload', (req, res) => {
+        upload(req, res, (err) => {
+            if(err) {
+                res.json({error_code: 1, err_desc: err})
+                return
+            }
+            res.json({error_code: 0, err_desc: null, file_uploaded: true})
+        })
+    })
+
+    app.get('/file/:filename', (req, res) => {
+        gfs.collection('ctFiles')
+        gfs.files.find({filename: req.params.filename}).toArray(function(err, files) {
+            if(!files || files.length == 0) {
+                return res.status(404).json({
+                    responseCode: 1,
+                    reponseMessage: "error"
+                })
+                var readstream = gfs.createReadStream({
+                    filename: files[0].filename,
+                    root: "ctFiles"
+                })
+                res.set('Content-Type', files[0].contentType)
+                return readstream.pipe(res)
+            }
+        })
+    })
+}
