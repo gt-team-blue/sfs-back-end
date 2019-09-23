35d00672f888eb29c3dcc6f6aeabe13a53c4aa79
Phillip Stephens <pstephens9275@gmail.com>
2019-09-23 14:25:44 -0400

    Added MongoShell connection string to keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,9 @@ module.exports = {
   mongoURI:
     "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/SFS_Backend"
 };
-
+//For MongoShell
+//Connection String: mongo "mongodb+srv://testcluster-w4ynb.mongodb.net/test" --username phil927
+//Password: 7nuaA_My3xcxetg
 module.exports = {
   mongoURI: "mongodb+srv://phil927:7nuaA_My3xcxetg@testcluster-w4ynb.mongodb.net/SFS_Backend",
   secretOrKey: "secret"
