4612f7d2d08b1f84553da42662ae1d2be7d166b2
Phillip Stephens <pstephens9275@gmail.com>
2019-09-25 23:10:52 -0400

    Added 4 methods to stories.js. GET stories, GET tags, GET storiesWithTags, GET storyByID. Should enable front-end filtering.

--- routes/api/stories.js
+++ routes/api/stories.js
@@ -3,56 +3,174 @@ const router = express.Router();
 // Load Story model
 const Story = require("../../models/Story");
 
-router.get('/story', (req, res) => {
-    Story.find({}).pretty().then(function(stories){
-        console.log("About to send");
-        res.send(stories);
+/*
+* This method will get all stories in database.
+* Example GET request: http://localhost:5000/api/stories/stories
+* Equivalent Query: db.getCollection('stories').find({});
+
+Example Return Packet
+{
+    "success": true/false   //Whether query succeeded
+    "data": [               //Story Metadata
+        {
+            "editAccess": [
+                "sszczepaniuk",
+                "magrawal"
+            ],
+            "tags": [
+                "Environmental",
+                "Intermediate"
+            ],
+            "_id": "5d7a734f1c9d440000832b37",
+            "title": "Story 1",
+            "creator": "sszczepaniuk",
+            "storyPointer": null,
+            "coverImagePointer": null,
+            "creationDate": "2019-09-04T18:41:15.459Z"
+        }
+    ]
+}
+*/
+
+router.get('/stories', (req, res) => {
+    Story.find({}, (err, stories) => {
+        if (err != null) {
+            console.log(err);
+            res.status(400).json({success: false, data: err});
+        } else {
+            res.status(200).json({success: true, data: stories});
+        }
     });
 });
 
 
 /*
-*This method will return all tags used on stores.
-*Intent is to use this method with filtering.
-*Example GET request: http://localhost:5000/api/stories/tags
-*Equivalent Query: db.getCollection('stories').distinct("tags")
+*   This method will return all tags used on stories.
+*   Intent is to use this method with filtering.
+
+*   Example GET request: http://localhost:5000/api/stories/tags
+*   Equivalent Query: db.getCollection('stories').distinct("tags")
+
+*   Returns array of tags
+*   {
+*       "success": true/false   //Whether query succeeded
+        "data": [               //Tag Array
+            "Easy",
+            "Politics"
+        ]
+    }
 */
 router.get('/tags', (req, res) => {
-    Story.distinct("tags").then(function(stories){
-        console.log("About to send");
-        res.send(stories);
+    Story.distinct("tags", (err, tags) => {
+        if (err != null) {
+            console.log(err);
+            res.status(400).json({success: false, data: err});
+        } else {
+            res.status(200).json({success: true, data: tags});
+        }
     });
 });
 
 /*
-db.getCollection('stories').find({
-    $or:[{
-        tags: "Easy"},
-   {tags: "Intermediate"}
-   ]})
-   */
-router.get('/storiesWithTags', (req, res) => {
-    console.log(req.body);
-    console.log("The tags are: " + req.body.tags.length);
-    console.log("The tags are: " + req.body.tags[0]);
-    console.log("The tags are: " + req.body.tags[1]);
-    var tags = "{";
-    for (var i = 0; i< req.body.tags.length; i++) {
-        tags += "tags: \"" + req.body.tags[i] + "\"}";
-        if (i < req.body.tags.length - 1) {
-            tags += ",{";
-        }
-    }
-    console.log(tags);
-    Story.find({
-    $or:[
-        {tags: "Easy"},{tags: "Environment"}
-       ]
+*This method will return all stories tagged with provided tags
+*Intent is to use this method with filtering.
 
-    }).then(function(stories){
-        console.log("About to send");
-        res.send(stories);
-    });
+*Example GET request: http://localhost:5000/api/stories/storiesWithTags
+*Example Body:
+*   tags: Easy
+*   tags: Envionmental
+
+Example Return Packet
+{
+    "success": true/false   //Whether query succeeded
+    "data": [               //Story Metadata
+        {
+            "editAccess": [
+                "sszczepaniuk",
+                "magrawal"
+            ],
+            "tags": [
+                "Environmental",
+                "Intermediate"
+            ],
+            "_id": "5d7a734f1c9d440000832b37",
+            "title": "Story 1",
+            "creator": "sszczepaniuk",
+            "storyPointer": null,
+            "coverImagePointer": null,
+            "creationDate": "2019-09-04T18:41:15.459Z"
+        }
+    ]
+}
+*/
+router.get('/storiesWithTags', (req, res) => {
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        Story.find({}, (err, story) => {
+            if (err != null) {
+                console.log(err);
+                res.status(400).json({success: false, data: err})
+            }
+            res.status(200).json({success: true, data: story});
+        });
+    } else if (req.body.tags.length > 0){
+        Story.find({tags: {$in: req.body.tags}},(err, story) => {
+            if (err != null) {
+                console.log(err);
+                res.status(400).json({success: false, data: err})
+            }
+            res.status(200).json({success: true, data: story});
+        });
+    } else {
+        res.status(400).json({success: false, data: "Error with length of tags input"});
+    }
 });
+
+/*
+This method will return the story specified by an ID
+
+Example GET request: http://localhost:5000/api/stories/storyFromId
+Example Body:
+   id: 5d7a734f1c9d440000832b37
+
+Returns array of story metadata
+Example Return Packet
+{
+    "success": true/false   //Whether query succeeded
+    "data": [               //Story Metadata
+        {
+            "editAccess": [
+                "sszczepaniuk",
+                "magrawal"
+            ],
+            "tags": [
+                "Environmental",
+                "Intermediate"
+            ],
+            "_id": "5d7a734f1c9d440000832b37",
+            "title": "Story 1",
+            "creator": "sszczepaniuk",
+            "storyPointer": null,
+            "coverImagePointer": null,
+            "creationDate": "2019-09-04T18:41:15.459Z"
+        }
+    ]
+}
+*/
+router.get('/storyFromId', (req, res) => {
+    if (req.body.id != null){
+        Story.find({_id: req.body.id},(err, story) => {
+            if (err != null) {
+                console.log(err);
+                res.status(400).json({success: false, data: err});
+            } else if (story.length == 0) {
+                res.status(400).json({success: false, data: "Story with that id not found"});
+            } else {
+                res.status(200).json({success: true, data: story});
+            }
+        });
+    } else {
+        res.status(400).json({success: false, data: "Please enter an id number"});
+    }
+})
 
 module.exports = router;
