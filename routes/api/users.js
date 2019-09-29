57f9cadffeeb3aaf309e212b61f36d8bf7263aaa
aidancbrady <me@aidancbrady.com>
2019-09-29 13:50:42 -0400

    Return user after login

--- routes/api/users.js
+++ routes/api/users.js
@@ -83,7 +83,8 @@ router.post("/login", (req, res) => {
              (err, token) => {
                res.json({
                  success: true,
-                 token: "Bearer " + token
+                 token: "Bearer " + token,
+                 user: user
                });
              }
            );
