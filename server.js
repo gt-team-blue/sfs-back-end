d3aa9df40b75ccea9ff6df8d425922f08a0923c0
aidancbrady <me@aidancbrady.com>
2019-09-29 13:43:52 -0400

    Fix access control origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 //allow cross origin requests
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
     next();
